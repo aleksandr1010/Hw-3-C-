0347b4bd46437ae674f35888a9e343c1f3e41a21
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-08-29 19:51:42 +0300

    Task_03

---
+++ Folder_03/Program.cs
@@ -0,0 +1,36 @@
+﻿// Написать программу замену элементов массива на противоположные
+// int n = new Random().Next(-100, 101);
+
+int n = 20;
+
+int[] newArray(int n)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = new Random().Next(-100, 101);
+    }
+    return array;
+}
+
+int[] Array(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * (-1);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine(   );
+}
+
+int[] array = newArray(n);
+PrintArray( array);
+PrintArray(Array(array));

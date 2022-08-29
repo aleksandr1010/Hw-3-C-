32b6e0f17227c66b71378ca208d74da5e8898822
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-08-29 19:49:09 +0300

    Task_02

---
+++ Folder_02/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задать массив из 12 элементов, заполненных числами из [-9,9]. 
+// Найти сумму положительных/отрицательных элементов массива
+
+int n = 12;
+int min = -9;
+int max = 9;
+
+int[] newArray(int n, int min, int max)
+{
+    int[] array = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+int[] Sums(int[] arr)
+{
+    int[] sums = new int[2];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < 0) sums[0] += arr[i];
+        else sums[1] += arr[i];
+    }
+    return sums;
+}
+
+void PritArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+       Console.Write($"{array[i]} ");
+    }
+       Console.WriteLine();
+}
+
+PritArray(Sums(newArray(n, min, max)));

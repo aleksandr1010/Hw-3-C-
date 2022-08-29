9a21431755b6b1767a0d0c016f22a3d2d722daa2
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-08-29 20:05:21 +0300

    Task_09

---
+++ Folder_09/Program.cs
@@ -0,0 +1,30 @@
+﻿//  В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+
+int size = new Random().Next(1, 11);
+
+Console.WriteLine($"Количетсов элементов массива: {size}");
+
+double[] SourceArray(int size)
+{
+    double[] array = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().NextDouble() * 100;
+    }
+    return array;
+}
+
+void PrintDiffMinMax(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine(arr[i]);
+    }
+    double difference = arr.Max() - arr.Min();
+    Console.WriteLine($"Минимальное значение массива {arr.Min()}, максимальное значение массива {arr.Max()}");
+    Console.WriteLine($"Разница между минимальным и максимальным значениями массива составляет {difference}");
+    Console.WriteLine();
+}
+
+PrintDiffMinMax(SourceArray(size));
+Console.WriteLine();

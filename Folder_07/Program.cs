d3384d478069df272080cab7c25a8bfd08acf763
Aleksandr Goncharenko <aleksandr.enzo@rambler.rugit config --global user.email aleksandr.enzo@rambler.ru>
2022-08-29 20:03:24 +0300

    Task_07

---
+++ Folder_07/Program.cs
@@ -0,0 +1,29 @@
+﻿//  Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+int size = 20;
+
+int[] FillArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    int sum = 0;
+   Console.WriteLine();
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write(arr[i] + " ");
+        if (i % 2 > 0) sum += arr[i];
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Сумма чисел, стоящих на нечетной позиции, соcтаваляет {sum} ");
+    Console.WriteLine();
+}
+
+PrintArray(FillArray(size));
